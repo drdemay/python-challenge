eb90f075e25d106b362b615ad7c2d2e09b119e8f
deanna.demay@gmail.com <deanna.demay@gmail.com>
2019-09-22 14:38:20 -0500

    commit files for homework submission

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,69 @@
-# This is main.py in PyBank
+import os
+import csv
+
+budgetdata_csv = os.path.join("budget_data.csv")
+
+# Open and read csv
+with open(budgetdata_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+# Skip header row
+    csv_header = next(csvreader)
+
+# Declare variables and starting value
+    months = 0
+    profitloss_total = 0
+    profitloss_averagechange = 0
+    profitloss_change = 0
+    profitloss_change_num = 0
+    month_greatest_inc = ""
+    greatest_inc = 0
+    month_greatest_dec = ""
+    greatest_dec = 0
+
+    previous_month = ""
+    previous_month_amt = 0
+
+    for row in csvreader:    
+        months += 1
+        profitloss_total += int(row[1])
+        
+        if previous_month != "":
+            profitloss_change = profitloss_change + (int(row[1]) - previous_month_amt)
+            profitloss_change_num += 1      
+            if int(row[1]) - previous_month_amt > greatest_inc:
+                month_greatest_inc = (row[0])
+                greatest_inc = int(row[1]) - previous_month_amt
+            if int(row[1]) - previous_month_amt < greatest_dec:
+                month_greatest_dec = row[0]
+                greatest_dec = int(row[1]) - previous_month_amt
+        previous_month = row[0]
+        previous_month_amt = int(row[1])
+    
+    profitloss_averagechange = profitloss_change / profitloss_change_num 
+
+# Print budget_data information
+    print("Financial Analysis")
+    print("-----------------------------")
+    print(f"Total Months: {int(months)}")
+    print(f"Total: ${int(profitloss_total)}")
+    print(f"Average Change: ${float(profitloss_averagechange):.2f}")
+    print(f"Greatest Increase in Profits: {str(month_greatest_inc)} ${int(greatest_inc)}")
+    print(f"Greatest Decrease in Profits: {str(month_greatest_dec)} (${int(greatest_dec)})")
+
+# save the output file path
+output_file = os.path.join("output.csv")
+
+# open the output file, create a header row, and then write the 
+with open(output_file, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
+
+    writer.writerow(["Financial Analysis"])
+    writer.writerow(["-----------------------------"])
+    writer.writerow([f"Total Months: {int(months)}"])
+    writer.writerow([f"Total: ${int(profitloss_total)}"])
+    writer.writerow([f"Average Change: ${float(profitloss_averagechange):.2f}"])
+    writer.writerow([f"Greatest Increase in Profits: {str(month_greatest_inc)} ${int(greatest_inc)}"])
+    writer.writerow([f"Greatest Decrease in Profits: {str(month_greatest_dec)} (${int(greatest_dec)})"])
+
+datafile.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,95 @@
 # This is main.py in PyPoll
-print("Hello, World!")
-print("There's no such thing as ... well, you know")
+import os
+import csv
+
+electiondata_csv = os.path.join("election_data.csv")
+
+# Open and read csv
+with open(electiondata_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+# Skip header row
+    csv_header = next(csvreader)
+
+# declare variables and starting values
+    votes = 0
+    candidate_count = 0
+    candidates = []
+    candidate_vote = []
+
+    winning_candidate = ""
+    winning_candidate_votes = 0
+
+    for row in csvreader:
+        votes += 1
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            candidate_vote.append(1)
+        id = 0
+        for candidate in candidates:
+            if row[2] == candidate:
+                candidate_vote[id] += 1
+            id += 1
+
+    print("Election Results")
+    print("-----------------------------")
+    print(f"Total Votes: {int(votes)}")
+    print("-----------------------------")
+    id = 0
+    for candidate in candidates: 
+        print(f"{(candidate)}",f"{(candidate_vote[id]/votes) * 100 :.3f}%",f"({(candidate_vote[id])})")
+        if winning_candidate == "": 
+            winning_candidate = candidate  
+        if candidate_vote[id] > winning_candidate_votes: 
+            winning_candidate = candidate  
+            winning_candidate_votes = candidate_vote[id]
+        id += 1
+    print("-----------------------------")
+    print(f"Winner: {winning_candidate}")
+    print("-----------------------------")
+
+
+# save the output file path
+output_file = os.path.join("output.csv")
+
+# open the output file, create a header row, and then write the 
+with open(output_file, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
+
+    writer.writerow(["Election Results"])
+    writer.writerow(["-----------------------------"])
+    writer.writerow([f"Total Votes: {int(votes)}"])
+    writer.writerow(["-----------------------------"])
+    id = 0
+    for candidate in candidates: 
+        writer.writerow([f"{(candidate)}",f"{(candidate_vote[id]/votes) * 100}%",f"{(candidate_vote[id])}"])
+        if winning_candidate == "": 
+            winning_candidate = candidate  
+        if candidate_vote[id] > winning_candidate_votes: 
+            winning_candidate = candidate  
+            winning_candidate_votes = candidate_vote[id]
+        id += 1
+    writer.writerow(["-----------------------------"])
+    writer.writerow([f"Winner: {winning_candidate}"])
+    writer.writerow(["-----------------------------"])
+
+datafile.close()
+
+# The total number of votes cast
+    # Add number of rows (minus header row - done above) to Calculate the total number of votes cast
+
+# A complete list of candidates who received votes
+    # Loop through list and each time a new candidate name is found add it to the list of candidates to print
+
+# A complete list of candidates who received votes
+    # done - Loop through list and each time a new candidate name is found  
+    # add it to the list of candidates to print
+
+# The total number of votes each candidate won
+    # Loop through list and each time a candidate is listed, add 1 to their vote total (The total number of votes each candidate won)
+
+# The percentage of votes each candidate won
+    # Divide the number of votes that a candidate won by the number of total votes and create a percentage * 100
+
+# The winner of the election based on popular vote.
+    # Loop through list of candidates and find the candidate with the lagest number of votes
